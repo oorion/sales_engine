38329191431d534150ac78a2aac35b17eebe26bc
Alex Robinson <alexandra.scott335@gmail.com>
2014-11-26 15:12:49 -0700

    added find by all methods to item and repo

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,9 +1,10 @@
 class Customer
-  attr_reader :id, :name, :created_at, :updated_at
+  attr_reader :id, :first_name, :last_name, :created_at, :updated_at
 
   def initialize(data)
     @id         = data[:id]
-    @name       = data[:name]
+    @first_name = data[:first_name]
+    @last_name  = data[:last_name]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
   end
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -5,13 +5,31 @@ class CustomerRepositoryTest < Minitest::Test
   def test_it_has_a_collection_of_customer_objects
     data = [{
       id: '23',
-      name: 'newname',
+      first_name: 'fistname',
+      last_name: 'lastname',
       created_at: '2012-11-28',
       updated_at: '2014-10-15'
     }].collect { |row| Customer.new(row) }
 
     customer_repository = CustomerRepository.new(data)
     assert_instance_of Array, customer_repository.entries
-    assert_instance_of Customer, customer_repository.entries[0]  
+    assert_instance_of Customer, customer_repository.entries[0]
+  end
+
+    def test_it_can_find_by_first_name
+      data = [{
+        id: '1',
+        first_name: 'Joey',
+        last_name: 'Ondricka',
+        created_at: '2012-03-27 14:54:09 UTC',
+        updated_at: '2012-03-27 14:54:09 UTC'},{
+        id: '2',
+        first_name: 'Cecelia',
+        last_name: 'Osinski',
+        created_at: '2012-03-27 14:54:10 UTC',
+        updated_at: '2012-03-27 14:54:10 UTC'
+          }].collect { |row| Customer.new(row) }
+    customer_repository = CustomerRepository.new(data)
+    assert_equal 'Joey', customer_repository.find_by_first_name('Joey')
   end
 end
