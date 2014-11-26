dc3e431d6bd5acf6e6cfa3240c6839470b31745c
Orion Osborn <orion.osborn@gmail.com>
2014-11-26 14:43:52 -0700

    add methods to item_repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -7,4 +7,39 @@ class ItemRepository
     @entries = entries
   end
 
+  def all
+    entries
+  end
+
+  def random
+    entries.sample
+  end
+
+  def find_by_id(match)
+    entries.find { |entry| entry.id == match }
+  end
+
+  def find_by_name(match)
+    entries.find { |entry| entry.name == match }
+  end
+
+  def find_by_description(match)
+    entries.find { |entry| entry.description == match }
+  end
+
+  def find_by_unit_price(match)
+    entries.find { |entry| entry.unit_price == match }
+  end
+
+  def find_by_merchant_id(match)
+    entries.find { |entry| entry.merchant_id == match }
+  end
+
+  def find_by_created_at(match)
+    entries.find { |entry| entry.created_at == match }
+  end
+
+  def find_by_updated_at(match)
+    entries.find { |entry| entry.updated_at == match }
+  end
 end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -2,18 +2,78 @@ require_relative 'test_helper'
 require_relative '../lib/item_repository'
 
 class ItemRepositoryTest < Minitest::Test
-  def test_it_has_a_collection_of_item_objects
-    data = [{
+  attr_reader :item_repository
+
+  def setup
+    data = [
+      {
       id: '1',
-      name: 'Item Qui Esse',
-      description: 'Nihil autem sit odio inventore deleniti. Est laudantium ratione distinctio laborum. Minus voluptatem nesciunt assumenda dicta voluptatum porro.',
+      name: 'name1',
+      description: 'Nihil autem',
       unit_price: '75107',
       merchant_id: '1',
       created_at: '2012-03-27 14:53:59 UTC',
       updated_at: '2012-03-27 14:53:59 UTC'
-    }].collect { |row| Item.new(row) }
-    item_repository = ItemRepository.new(data)
+      },
+      {
+        id: '2',
+        name: 'name2',
+        description: 'inventore deleniti',
+        unit_price: '751',
+        merchant_id: '2',
+        created_at: '2012-03-27 14:53:59 UTC',
+        updated_at: '2012-03-27 14:53:59 UTC'
+      }
+      ].collect { |row| Item.new(row) }
+      @item_repository = ItemRepository.new(data)
+  end
+
+  def test_it_has_a_collection_of_item_objects
     assert_instance_of Array, item_repository.entries
     assert_instance_of Item, item_repository.entries[0]
+  end
+
+  def test_can_return_all_items
+    assert_equal 2, item_repository.all.count
+  end
+
+  def test_can_return_random_item
+    assert_instance_of Item, item_repository.random
+  end
+
+  def test_can_find_by_id
+    assert_equal "1", item_repository.find_by_id('1').id
+  end
+
+  def test_can_find_by_name
+    assert_equal 'name1', item_repository.find_by_name('name1').name
+  end
+
+  # def test_can_find_by_name_as_uppercase
+  #   assert_equal 'name1', item_repository.find_by_name('NAME1').name
+  # end
+
+  def test_can_find_by_description
+    assert_equal 'Nihil autem',
+    item_repository.find_by_description('Nihil autem').description
+  end
+
+  def test_can_find_by_unit_price
+    assert_equal '75107',
+    item_repository.find_by_unit_price('75107').unit_price
+  end
+
+  def test_can_find_by_merchant_id
+    assert_equal '1', item_repository.find_by_merchant_id('1').merchant_id
+  end
+
+  def test_can_find_by_created_at
+    assert_equal '2012-03-27 14:53:59 UTC',
+    item_repository.find_by_created_at('2012-03-27 14:53:59 UTC').created_at
+  end
+
+  def test_can_find_by_updated_at
+    assert_equal '2012-03-27 14:53:59 UTC',
+    item_repository.find_by_updated_at('2012-03-27 14:53:59 UTC').updated_at
   end
 end
