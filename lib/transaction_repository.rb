6648a90be586cccbff98a84fff6140d3cf0f3471
Alex Robinson <alexandra.scott335@gmail.com>
2014-11-28 10:01:01 -0700

    added methods to transaction and repo

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -6,4 +6,64 @@ class TransactionRepository
   def initialize(entries = [])
       @entries = entries
   end
+
+  def all
+    entries
+  end
+
+  def random
+    entries.sample
+  end
+
+  def find_by_id(match)
+    entries.find { |entry| entry.id == match }
+  end
+
+  def find_by_invoice_id(match)
+    entries.find { |entry| entry.invoice_id == match }
+  end
+
+  def find_by_credit_card_number(match)
+    entries.find { |entry| entry.credit_card_number == match }
+  end
+
+  def find_by_credit_card_expiration_date(match)
+    entries.find { |entry| entry.credit_card_expiration_date == match }
+  end
+
+  def find_by_result(match)
+    entries.find { |entry| entry.result.downcase == match.downcase }
+  end
+
+  def find_by_created_at(match)
+    entries.find { |entry| entry.created_at.downcase == match.downcase }
+  end
+
+  def find_by_updated_at(match)
+    entries.find { |entry| entry.updated_at.downcase == match.downcase }
+  end
+
+  def find_all_by_id(match)
+    entries.select { |entry| entry.id == match }
+  end
+
+  def find_all_by_credit_card_number(match)
+    entries.select { |entry| entry.credit_card_number == match }
+  end
+
+  def find_all_by_credit_card_expiration_date(match)
+    entries.select { |entry| entry.credit_card_expiration_date == match }
+  end
+
+  def find_all_by_result(match)
+    entries.select { |entry| entry.result.downcase == match.downcase }
+  end
+
+  def find_all_by_created_at(match)
+    entries.select { |entry| entry.created_at.downcase == match.downcase }
+  end
+
+  def find_all_by_updated_at(match)
+    entries.select { |entry| entry.updated_at.downcase == match.downcase }
+  end
 end
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -2,7 +2,9 @@ require_relative 'test_helper'
 require_relative '../lib/transaction_repository'
 
 class TransactionRepositoryTest < Minitest::Test
-  def test_it_has_a_collection_of_transaction_instances
+  attr_reader :transaction_repository
+
+  def setup
     data = [{
       id: '50',
       invoice_id: '50',
@@ -11,9 +13,95 @@ class TransactionRepositoryTest < Minitest::Test
       result: 'success',
       created_at: '2012-11-28',
       updated_at: '2014-10-15'
-    }].collect { |row| Transaction.new(row) }
-    transaction_repository = TransactionRepository.new(data)
+      },
+      {
+        id: '49',
+        invoice_id: '49',
+        credit_card_number: '4540842003561940',
+        credit_card_expiration_date: '2014-11-26',
+        result: 'success',
+        created_at: '2012-11-18',
+        updated_at: '2014-10-25'
+      },
+      {
+        id: '50',
+        invoice_id: '50',
+        credit_card_number: '4540842003561938',
+        credit_card_expiration_date: '2014-11-25',
+        result: 'success',
+        created_at: '2012-11-28',
+        updated_at: '2014-10-15'
+      }].collect { |row| Transaction.new(row) }
+      @transaction_repository = TransactionRepository.new(data)
+  end
+
+  def test_it_has_a_collection_of_transaction_instances
     assert_instance_of Array, transaction_repository.entries
     assert_instance_of Transaction, transaction_repository.entries[0]
   end
+
+  def test_can_return_all_transactions
+    assert_equal 3, transaction_repository.all.count
+  end
+
+  def test_can_return_a_random_transaction
+    assert_instance_of Transaction, transaction_repository.random
+  end
+
+  def test_can_find_by_id
+    assert_equal '50', transaction_repository.find_by_id('50').id
+  end
+
+  def test_can_find_by_invoice_id
+    assert_equal '49', transaction_repository.find_by_invoice_id('49').invoice_id
+  end
+
+  def test_can_find_by_credit_card_number
+    assert_equal '4540842003561938',
+    transaction_repository.find_by_credit_card_number('4540842003561938').credit_card_number
+  end
+
+  def test_can_find_by_credit_card_expiration_date
+    assert_equal '2014-11-25',
+    transaction_repository.find_by_credit_card_expiration_date('2014-11-25').credit_card_expiration_date
+  end
+
+  def test_can_find_by_result
+    assert_equal 'success', transaction_repository.find_by_result('success').result
+  end
+
+  def test_can_find_by_created_at
+    assert_equal '2012-11-28', transaction_repository.find_by_created_at('2012-11-28').created_at
+  end
+
+  def test_can_find_by_updated_at
+    assert_equal '2014-10-25', transaction_repository.find_by_updated_at('2014-10-25').updated_at
+  end
+
+  def test_can_find_all_by_id
+    assert_equal 2, transaction_repository.find_all_by_id('50').count
+  end
+
+  def test_can_find_all_by_credit_card_number
+    assert_equal 2,
+    transaction_repository.find_all_by_credit_card_number('4540842003561938').count
+  end
+
+  def test_can_find_all_by_credit_card_expiration_date
+    assert_equal 2,
+    transaction_repository.find_all_by_credit_card_expiration_date('2014-11-25').count
+  end
+
+  def test_can_find_all_by_result
+    assert_equal 3, transaction_repository.find_all_by_result('success').count
+  end
+
+  def test_can_find_all_by_created_at
+    assert_equal 2, transaction_repository.find_all_by_created_at('2012-11-28').count
+  end
+
+  def test_can_find_all_by_updated_at
+    assert_equal 1, transaction_repository.find_all_by_updated_at('2014-10-25').count
+  end
+
 end
